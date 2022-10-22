19c6ea866c4522c8c7334e617f38ac0ba6057f3d
Visage <49914133+Visaging@users.noreply.github.com>
2022-10-22 22:48:47 +0530

    Version 6.4

--- Cruise_Control_Remaster.lua
+++ Cruise_Control_Remaster.lua
@@ -1,8 +1,8 @@
 script_name("Cruise Control Remaster")
 script_author("Visage A.K.A. Ishaan Dunne")
 
-local script_version = 6.31
-local script_version_text = '6.31'
+local script_version = 6.4
+local script_version_text = '6.4'
 
 require "moonloader"
 require "sampfuncs"
